91c5fd7a69622ea2fcd65171c17991315002d5e2
Hayley Davies <cd230099@falmouth.ac.uk>
2020-12-14 18:13:38 +0000

    updated load-content.js so it loads locally (better for testing) added error checking to load-content.js in the event of file not existing. if the file can't be found, it will instead display "Whoops! The [content] couldn't be loaded!"

--- javascript/load-content.js
+++ javascript/load-content.js
@@ -1,12 +1,18 @@
-fetch("https://hayleydavi.es/loads/header.html")
+fetch("loads/header.html")
 .then(response => {
+    if (response.status != 200) {
+        return "Whoops. The header couldn't be loaded!"
+    }
     return response.text()
 })
 .then(data => {
     document.getElementById("header").innerHTML = data;
 });
-fetch("https://hayleydavi.es/loads/footer.html")
+fetch("loads/footer.html")
 .then(response => {
+    if (response.status != 200) {
+        return "Whoops. The footer couldn't be loaded!"
+    }
     return response.text()
 })
 .then(data => {
