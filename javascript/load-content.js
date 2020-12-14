b38ce3f31aec375758a19c45f0dd3786c422f4b8
Hayley Davies <cd230099@falmouth.ac.uk>
2020-12-14 17:22:56 +0000

    added footer and footer loading

--- javascript/load-content.js
+++ javascript/load-content.js
@@ -5,3 +5,11 @@ fetch("https://hayleydavi.es/header.html")
 .then(data => {
     document.getElementById("header").innerHTML = data;
 });
+fetch("https://hayleydavi.es/footer.html")
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+window.scrollTo(0, 0);
