47e239cfdca71ef1e851f054a64c8e671cbee397
Hayley Davies <cd230099@falmouth.ac.uk>
2022-04-23 20:35:29 +0100

    Updated layout of page
    - Page now has a hidden section under games which gets displayed when the dropdown is clicked.

--- js/page.js
+++ js/page.js
@@ -2,7 +2,6 @@ const listOfTransitionElementClasses = [
     "section-split"
 ];
 
-
 for (let elementClass of listOfTransitionElementClasses) {
     let pageSections = document.getElementsByClassName(elementClass);
 
@@ -11,16 +10,24 @@ for (let elementClass of listOfTransitionElementClasses) {
 
         // Create the observer, same as before:
         let observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                element.classList.add(elementClass + "-transition");
-                return;
-            }
-
-            //element.classList.remove(elementClass + "-transition");
-        });
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    element.classList.add(elementClass + "-transition");
+                    return;
+                }
+            });
         });
 
         observer.observe(element);
+    }
+}
+
+function togglePanel(id) {
+    var contentToToggle = document.getElementById(id);
+
+    if (contentToToggle.classList.contains("promo-panel-hidden")) {
+        contentToToggle.classList.remove("promo-panel-hidden");
+    } else {
+        contentToToggle.classList.add("promo-panel-hidden");
     }
 }
