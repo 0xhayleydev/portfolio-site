0f123d39e0531036d81128220e5aea5aafe5d224
Hayley Davies <cd230099@falmouth.ac.uk>
2022-12-30 20:51:36 +0000

    update: added information to flight-attendant-simulator

--- js/project-page.js
+++ js/project-page.js
@@ -13,22 +13,57 @@ fetch('./json/projects.json')
 
 function loadProject(json) {
     let projectJson = json[project];
-    createNewSection();
-    let sections = document.getElementsByClassName("page-section");
-    addInitialContent(sections[0], projectJson);
 
-    for(let i = 0; i < projectJson.sections.length; i++) {
-        createNewSection();
+    pageBody.appendChild(addInitialContent(projectJson));
+
+    let projectSections = projectJson["sections"];
+    for(let i = 0; i < projectSections.length; i++) {
+        let section = addContent(projectSections[i]);
+        pageBody.appendChild(section);
     }
+}
+
+function addContent(sectionJson) {
+    let section = createNewSection();
+    let subsection = createNewSection();
+    subsection.className = "section-container";
+    subsection.appendChild(getHeading(sectionJson));
+    subsection.appendChild(getImage(sectionJson));
+    subsection.appendChild(getDescription(sectionJson));
+    section.appendChild(subsection);
+    return section;
+}
+
+function getHeading(sectionJson) {
+    let h1 = createElement("h1");
+    h1.className = "project-heading";
+    h1.innerHTML = sectionJson["heading"];
+    return h1;
+}
+
+function getImage(sectionJson) {
+    let img = createElement("img");
+    img.className = "project-image";
+    img.src = sectionJson["image"];
+    return img;
+}
+
+function getDescription(sectionJson) {
+    let p = createElement("p");
+    p.className = "section-description"
+    p.innerHTML = sectionJson["description"];
+    return p;
 }
 
 function createNewSection() {
     let div = createElement("div");
     div.className = "page-section";
-    pageBody.appendChild(div);
+    return div;
 }
 
-function addInitialContent(section, projectJson) {
+function addInitialContent(projectJson) {
+    let section = createNewSection();
+    
     let img = createElement("img");
     img.src = projectJson["image"];
     img.className = "promo-image";
@@ -37,6 +72,24 @@ function addInitialContent(section, projectJson) {
     let h1 = createElement("h1");
     h1.innerHTML = projectJson["name"];
     section.appendChild(h1);
+    
+    section.appendChild(getIcons(projectJson))
+    return section;
+}
+
+
+
+function getIcons(projectJson) {
+    let icons = projectJson["icons"];
+
+    let div = createElement('div')
+    div.classList.add('social-links')
+    for(let icon = 0; icon < icons.length; icon++) {
+        let i = createElement('i');
+        i.classList.add(icons[icon]);
+        div.appendChild(i);
+    }
+    return div;
 }
 
 function createElement(type) {
